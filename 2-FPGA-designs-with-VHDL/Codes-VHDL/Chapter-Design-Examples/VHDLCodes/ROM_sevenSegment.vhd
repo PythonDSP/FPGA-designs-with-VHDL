491f5884336327094d713b0f263d30660172dcda
pythondsp <mpat260@aucklanduni.ac.nz>
2017-07-07 11:37:14 +1200

    ROM seven segment code

--- 2-FPGA-designs-with-VHDL/Codes-VHDL/Chapter-Design-Examples/VHDLCodes/ROM_sevenSegment.vhd
+++ 2-FPGA-designs-with-VHDL/Codes-VHDL/Chapter-Design-Examples/VHDLCodes/ROM_sevenSegment.vhd
@@ -34,23 +34,23 @@ architecture arch of ROM_sevenSegment is
 	type rom_type is array (0 to addr_width-1) of std_logic_vector(data_width-1 downto 0);
 	
 	signal sevenSegment_ROM : rom_type := (
-								"0000001",  -- 0 : location 0
-								"1001111",  -- 1 : location 1
-								"0010010",  -- 2 : location 2
-								"0000110",  -- 3 : location 3
-								"1001100",  -- 4 : location 4
-								"0100100",  -- 5 : location 5
-								"0100000",  -- 6 : location 6
-								"0001111",  -- 7 : location 7
-								"0000000",  -- 8 : location 8
-								"0000100",  -- 9 : location 9
-								"0001000",  -- A : location 10
-								"1100000",  -- B : location 11
-								"0110001",  -- C : location 12
-								"1000010",  -- D : location 13
-								"0110000",  -- E : location 14
-								"0111000"   -- F : location 15
-							);
+							"1000000", -- 0, active low i.e. 0:display & 1:no display
+							"1111001", -- 1
+							"0100100", -- 2
+							"0110000", -- 3
+							"0011001", -- 4
+							"0010010", -- 5
+							"0000010", -- 6
+							"1111000", -- 7
+							"0000000", -- 8
+							"0010000", -- 9
+							"0001000", -- a
+							"0000011", -- b
+							"1000110", -- c
+							"0100001", -- d
+							"0000110", -- e
+							"0001110"  -- f
+		);
 begin
 	data <= sevenSegment_ROM(to_integer(unsigned(addr)));
 end arch; 
